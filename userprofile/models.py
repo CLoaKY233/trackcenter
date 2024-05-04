ac4d138411b2d8682d9ebf1612a07540a80146c1
CLoaKY233 <laysheth1@gmail.com>
2024-05-04 22:27:58 +0530

    profiles working (display)

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class userprofile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    user_githubid=models.CharField(max_length=70,default="NOT PROVIDED")
+    project_name=models.CharField(max_length=25,default="NOT PROVIDED")
+    project_about=models.CharField(max_length=300,default="NOT PROVIDED")
+    project_link=models.CharField(max_length=100,default="NOT PROVIDED")
+
+    def __str__(self):
+        return self.user.username
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from .models import userprofile
 # Create your views here.
 def profile(request):
     user = request.user
+    info = userprofile.objects.get(user=user)
     user_profile = {
         'username': user.username,
         'fname': user.first_name,
         'lname': user.last_name,
-        'github_id': '1990-01-01',
-        'project_name': 'https://github.com/john_doe',
-        'project_link': 'Sample Project',
-        'project_about': 'https://github.com/john_doe/sample_project',
-        'email_address': 'Sample Project',
+        'github_id': info.user_githubid,
+        'project_name': info.project_name,
+        'project_link': info.project_link,
+        'project_about': info.project_about,
+        'email_address': user.email,
 
     }
 
