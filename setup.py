7b53b92928acabbddb403eb432a8c5263c3a320e
CLoaKY233 <laysheth1@gmail.com>
2024-05-06 12:15:21 +0530

    email issue fix

--- setup.py
+++ setup.py
@@ -7,16 +7,24 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Django',
-        'gunicorn',
-        'dj-database-url',
-        'psycopg2',
-        'whitenoise',
-        'django-heroku',
         'django-crispy-forms',
         'django-bootstrap4',
         'django-allauth',
         'django-countries',
+        'asgiref'
+        'dj-database-url'
+        'Django'
+        'django-heroku'
+        'et-xmlfile'
+        'gunicorn'
+        'openpyxl'
+        'packaging'
+        'psycopg2'
+        'setuptools'
+        'sqlparse'
+        'typing_extensions'
+        'tzdata'
+        'whitenoise'      
     ],
     entry_points={
         'console_scripts': [
